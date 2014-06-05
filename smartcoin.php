07c655294ed03e7ad9069dce69cbdbe164fde5af
agranado <agranado2k@gmail.com>
2014-06-05 16:30:49 -0300

    Anti Fraud System Integration

--- smartcoin.php
+++ smartcoin.php
@@ -16,3 +16,43 @@ function smartcoin_init_your_gateway() {
 }
 
 add_action('plugins_loaded', 'smartcoin_init_your_gateway',0);
+
+wp_enqueue_script( 'sift_science', sift_js(), array( 'jquery' ));
+
+function sift_js() {
+	create_user_session();
+	$smartcoin_sys_id = 'bf65ad0591';
+
+	echo "
+	<script>
+		var _user_id = '';
+	  var _session_id = '" . $_COOKIE['smartcoin_sys'] . "'; 
+
+	  var _sift = _sift || [];
+	  _sift.push(['_setAccount', '" . $smartcoin_sys_id . "']);
+	  _sift.push(['_setUserId', _user_id]);
+	  _sift.push(['_setSessionId', _session_id]);
+	  _sift.push(['_trackPageview']);
+	  (function() {
+	    function ls() {
+	      var e = document.createElement('script');
+	      e.type = 'text/javascript';
+	      e.async = true;
+	      e.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.siftscience.com/s.js';
+	      var s = document.getElementsByTagName('script')[0];
+	      s.parentNode.insertBefore(e, s);
+	    }
+	    if (window.attachEvent) {
+	      window.attachEvent('onload', ls);
+	    } else {
+	      window.addEventListener('load', ls, false);
+	    }
+	  })();
+	</script>";
+}
+
+function create_user_session() {
+  if ( !is_admin() && !isset($_COOKIE['smartcoin_sys'])) {
+      setcookie('smartcoin_sys', uniqid('sc_'), 0, COOKIEPATH, COOKIE_DOMAIN, false);
+  }
+}
--- smartcoin_gateway.php
+++ smartcoin_gateway.php
@@ -28,6 +28,13 @@ class SmartCoin extends WC_Payment_Gateway {
     $this->api_key          = $this->use_test_api ? $this->test_api_key : $this->live_api_key;
     $this->api_secret       = $this->use_test_api ? $this->test_api_secret : $this->live_api_secret;
 
+    //tmp fields
+    $this->test_sift_account = $this->get_option('test_sift_account');
+    $this->test_sift_api_key = $this->get_option('test_sift_api_key');
+    $this->live_sift_account = $this->get_option('live_sift_account');
+    $this->live_sift_api_key = $this->get_option('live_sift_api_key');
+    $this->sift_account      = $this->use_test_api ? $this->test_sift_account : $this->live_sift_account;
+    $this->sift_api_key      = $this->use_test_api ? $this->test_sift_api_key : $this->live_sift_api_key;
 
 
     add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
@@ -81,6 +88,27 @@ class SmartCoin extends WC_Payment_Gateway {
             'type'        => 'text',
             'title'       => __('Live API Secret', 'woothemes'),
             'default'     => __('','woothemes')
+          ),
+        //temp field
+        'test_sift_account' => array(
+            'type'        => 'text',
+            'title'       => __('Test Sift Account', 'woothemes'),
+            'default'     => __('','woothemes')
+          ),
+        'test_sift_api_key' => array(
+            'type'        => 'text',
+            'title'       => __('Test Sift API Key', 'woothemes'),
+            'default'     => __('','woothemes')
+          ),
+        'live_sift_account' => array(
+            'type'        => 'text',
+            'title'       => __('Live Sift Account', 'woothemes'),
+            'default'     => __('','woothemes')
+          ),
+        'live_sift_api_key' => array(
+            'type'        => 'text',
+            'title'       => __('Live Sift API Key', 'woothemes'),
+            'default'     => __('','woothemes')
           )
       );
   }
@@ -119,6 +147,7 @@ class SmartCoin extends WC_Payment_Gateway {
     $data = $this->get_params();
 
     try {
+      $this->send_data_to_sift();
       $charge = Charge::create(array(
                   "amount"      => $data['amount'], // amount in cents, again
                   "currency"    => $data['currency'],
@@ -146,7 +175,7 @@ class SmartCoin extends WC_Payment_Gateway {
   protected function get_params() {
     if ($this->order AND $this->order != null) {
       return array(
-          "amount"      => (float)$this->order->get_total() * 100,
+          "amount"      => $this->order->get_total() * 100,
           "currency"    => strtolower(get_woocommerce_currency()),
           "token"       => $_POST['smartcoin_token'],
           "description" => sprintf("Pagamento para %s", $this->order->billing_email),
@@ -161,6 +190,91 @@ class SmartCoin extends WC_Payment_Gateway {
           )
       );
     }
+    return false;
+  }
+
+  protected function send_data_to_sift() {
+    if ($this->order AND $this->order != null) {
+      $items_list = array();
+      error_log('Sift order params:' . $this->order->id . "\n");
+      foreach ($this->order->get_items() as $i ) {
+        $item = array(
+          '$item_id'        => $i->item_meta,
+          '$product_title'  => $i->name,
+          '$price'          => $i->line_total * 1000000,
+          //'$upc'            => '097564307560',
+          //'$sku'            => '03586005',
+          //'$brand'          => 'Peters Kettle Corn',
+          //'$category'       => 'Food and Grocery',
+          '$quantity'       => $i->qty
+        );
+
+        error_log('Sift item params:' . implode (',' , $item) . '\n');
+        $items_list[] = $item;
+      }
+
+      error_log('Sift item list:' . implode (',' ,$items_list) . '\n');
+
+      $sift_params = array(
+        '$type'             => '$create_order',
+        '$api_key'          => $this->sift_api_key,
+        '$session_id'       => $_COOKIE['smartcoin_sys'],
+        '$user_id'          => $this->order->billing_email,
+        '$order_id'         => $this->order->id,
+        '$user_email'       => $this->order->billing_email,
+        '$amount'           => $this->order->get_total() * 1000000,
+        '$currency_code'    => strtolower(get_woocommerce_currency()),
+        '$billing_address'  => array(
+            '$name'             => sprintf("%s %s", $this->order->billing_first_name, $this->order->billing_last_name),
+            '$address_1'         => $this->order->billing_address_1,
+            '$address_2'         => $this->order->billing_address_2,
+            '$city'             => $this->order->billing_city,
+            '$region'           => $this->order->billing_state,
+            '$country'          => $this->order->billing_country,
+            '$zipcode'          => $this->order->billing_postcode,
+            '$phone'            => $this->order->billing_phone
+          ),
+        '$payment_methods'  => array(
+            '$payment_type'     => '$credit_card',
+            '$payment_gateway'  => '$stripe',
+            '$card_last4'       => $_POST['card_last4']
+          ),
+        '$shipping_address' => array(
+            '$address_1'         => $this->order->shipping_address_1,
+            '$address_2'         => $this->order->shipping_address_2,
+            '$city'             => $this->order->shipping_city,
+            '$region'           => $this->order->shipping_state,
+            '$country'          => $this->order->shipping_country,
+            '$zipcode'          => $this->order->shipping_postcode
+          ),
+        '$expedited_shipping' => true,
+        '$items'              => $items_list,
+        '$is_first_time_buyer' => false 
+      );
+
+      error_log('Sift params before:' . implode (',' ,$sift_params) . "\n");
+      error_log('Sift params billing:' . implode (',' ,$sift_params['$billing_address']) . "\n");
+      error_log('Sift params shipping:' . implode (',' ,$sift_params['$shipping_address']) . "\n");
+      error_log('Sift params payment:' . implode (',' ,$sift_params['$payment_methods']) . "\n");
+      error_log('Sift params items:' . implode (',' ,$sift_params['$items'][0]) . "\n");
+
+      $url = 'https://api.siftscience.com/v203/events';
+      $ch = curl_init($url);
+       
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $sift_params);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+
+      error_log('Sift params:' . implode (',' ,$sift_params) . "\n");
+      if($response = curl_exec($ch)) {
+        unset($_COOKIE['smartcoin_sys']);
+        setcookie('smartcoin_sys', null, -1, '/');
+        error_log('Sift response: ' . $response);  
+      }
+      curl_close($ch);
+      return true;
+    }
+    
     return false;
   }
 
